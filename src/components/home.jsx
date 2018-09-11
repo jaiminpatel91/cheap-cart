49a6cdcd20cefa334b68abceb6f3610bb56d6fbe
Jaimin <jaiminpatel91@gmail.com>
2018-09-11 19:39:58 -0400

    scss set eslint set

---
+++ src/components/home.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Im  here</h1>
+            </div>
+        )
+    }
+}
