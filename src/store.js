2aee004942e57dd2b254ea16277d19822aa03be9
Jaimin <jaiminpatel91@gmail.com>
2018-11-26 21:56:13 -0500

    add redux

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import reducer  from './reducers/appReducer';
+
+const initialState = {
+    name: 'jaimin'
+};
+
+const loggerMiddleware = createLogger();
+const middleware = [thunk];
+const enhancer = applyMiddleware(...middleware, loggerMiddleware);
+const store = createStore(reducer, initialState, enhancer);
+export default store;
