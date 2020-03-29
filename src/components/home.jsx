227a2ca54a182b6c089cfed605724a0955651cfc
jaiminpatel91 <jaiminpatel91@gmail.com>
2020-03-29 16:06:33 -0400

    let me try with new windows

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,7 +5,7 @@ export class Home extends Component {
     render() {
         return (
             <div>
-                <h1>Im  here..</h1>
+                <h1>Im  here jaimin</h1>
             </div>
         )
     }
