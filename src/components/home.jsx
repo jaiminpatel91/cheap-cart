65c481fea997949bde4eaf9969272df3c5745cf9
jaiminpatel91 <jaiminpatel91@gmail.com>
2020-03-29 16:46:22 -0400

    - tab icon change.

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export class Home extends Component {
 
     render() {
         return (
             <div>
-                <h1>Im  here..</h1>
+                <h1>Im  here.. jaimin</h1>
+                <FontAwesomeIcon icon={faHome} />
             </div>
         )
     }
