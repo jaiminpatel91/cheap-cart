7dff6937722184ae33b2c98a5ea0370d6e0b0a19
jaiminpatel91 <jaiminpatel91@gmail.com>
2020-03-30 21:04:11 -0400

    - add redux succesfully.

--- src/reducers/appReducer.js
+++ src/reducers/appReducer.js
@@ -3,7 +3,7 @@ import { ADD_TOKEN } from '../actions/appAction';
 const reducer = function (state, action) {
     switch (action.type) {
         case ADD_TOKEN:
-            return { ...state, name: 'patel' };
+            return { ...state, name: 'Don is don' };
 
         default:
             return state;
